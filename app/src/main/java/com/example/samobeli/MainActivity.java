f6b3559e02c4cc7bcaf3d30c855f0bc559785b1b
rivaldynaiborhu <rivaldynaiborhu@gmail.com>
2021-01-05 00:26:56 +0900

    add splash screen, add main activity

---
+++ app/src/main/java/com/example/samobeli/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.samobeli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToSignUp(View view) {
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
